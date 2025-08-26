80d905795b4c4cf2befc3977def8887f2674c084
CNC5 <github@cnc5.dev>
2025-08-26 19:55:44 +0300

    [feature]: required packages installation

--- build.sh
+++ build.sh
@@ -1,14 +1,30 @@
-#!/usr/bin/bash
+#!/bin/sh
 
 go install google.golang.org/protobuf/cmd/protoc-gen-go@latest
 go install google.golang.org/grpc/cmd/protoc-gen-go-grpc@latest
+
 GOPATH_BIN="$(go env GOPATH)/bin"
 if [[ ":$PATH:" != *":$GOPATH_BIN:"* ]]; then
     export PATH="$GOPATH_BIN:$PATH"
 fi
 
-mkdir -p gen
+if [ -x "$(command -v apk)" ];
+then
+    sudo apk add --no-cache protobuf
+elif [ -x "$(command -v apt-get)" ];
+then
+    sudo apt-get install protobuf-compiler
+#elif [ -x "$(command -v dnf)" ];
+#then
+#    sudo dnf install ""
+#elif [ -x "$(command -v zypper)" ];
+#then
+#    sudo zypper install ""
+else
+    echo "FAILED TO INSTALL PACKAGE: Package manager not found. You must manually install: 'protobuf'">&2;
+fi
 
+mkdir -p gen
 protoc -I proto \
     --go_out=gen --go_opt=paths=source_relative \
     --go-grpc_out=gen --go-grpc_opt=paths=source_relative \
